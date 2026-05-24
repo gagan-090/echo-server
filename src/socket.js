3ccb2c59e17efa8ed773245ff0df6074e5489682
CodingWith1T <gaganshuklarmg@gmail.com>
2026-05-24 23:48:50 +0530

    Fix socket.io CORS hardcoding

--- src/socket.js
+++ src/socket.js
@@ -12,7 +12,7 @@ let io;
 export const initSocket = (httpServer) => {
   io = new Server(httpServer, {
     cors: {
-      origin: 'http://localhost:5173', // Vite default
+      origin: env.CORS_ORIGIN,
       methods: ['GET', 'POST'],
       credentials: true
     }
